3cab7690d3ceac2ddad15d31532d5e2fec641381
pedro <pedro.jcaceresl@gmail.com>
2022-12-10 20:59:30 -0300

    Binary Search Tree

--- Data Structures/Binary Search Tree/BinarySearchTree.js
+++ Data Structures/Binary Search Tree/BinarySearchTree.js
@@ -37,13 +37,13 @@ class BinarySearchTree {
   }
 
   find(value) {
-    let currenNode = this.root;
+    let currentNode = this.root;
 
-    while (currenNode) {
-      if (value === currenNode.value) return currenNode;
+    while (currentNode) {
+      if (value === currentNode.value) return currentNode;
 
-      if (value < currenNode.left) currenNode = currenNode.left;
-      else currenNode = currenNode.right;
+      if (value < currentNode.left) currentNode = currentNode.left;
+      else currentNode = currentNode.right;
     }
     return undefined;
   }
@@ -56,11 +56,11 @@ class BinarySearchTree {
     stack.push(node);
 
     while (stack.length) {
-      let currenNode = stack.pop();
+      let currentNode = stack.pop();
 
-      if (currenNode.right) stack.push(currenNode.right);
-      if (currenNode.left) stack.push(currenNode.left);
-      result.push(currenNode.value);
+      if (currentNode.right) stack.push(currentNode.right);
+      if (currentNode.left) stack.push(currentNode.left);
+      result.push(currentNode.value);
     }
     return result;
   }
@@ -89,10 +89,10 @@ class BinarySearchTree {
 
     stack.push(node);
     while (stack.length) {
-      let currenNode = stack.pop();
-      if (currenNode.left) stack.push(currenNode.left);
-      if (currenNode.right) stack.push(currenNode.right);
-      result.push(currenNode.value);
+      let currentNode = stack.pop();
+      if (currentNode.left) stack.push(currentNode.left);
+      if (currentNode.right) stack.push(currentNode.right);
+      result.push(currentNode.value);
     }
     return result.reverse();
   }
@@ -114,20 +114,153 @@ class BinarySearchTree {
   }
 
   findNodeWithParent(value) {
-    let parentNode = null;
+    let parent = null;
     let currentNode = this.root;
 
     while (currentNode) {
-      if (value === currentNode.value) break;
-
-      parentNode = currentNode;
-
+      parent = currentNode;
+      // test for children
       if (value < currentNode.value) currentNode = currentNode.left;
       else currentNode = currentNode.right;
+
+      if (value === currentNode?.value) return { parent, currentNode };
     }
-    return { parentNode, currentNode };
+  }
+
+  findNextBigNodeWithParent(node = this.root) {
+    let nextBigNodeParent = node;
+
+    if (!nextBigNodeParent || !nextBigNodeParent.right) {
+      return { nextBigNodeParent, nextBigNode: null };
+    }
+    let nextBigNode = node.right;
+
+    while (nextBigNode.left) {
+      nextBigNodeParent = nextBigNode;
+      nextBigNode = nextBigNode.left;
+    }
+    return { nextBigNodeParent, nextBigNode };
+  }
+
+  //              10
+  //             /  \
+  //           8     12
+  //         /  \   / \
+  //        5   9  11  20
+  //      /  \        /  \
+  //     2    6      18  25
+
+  remove1(value) {
+    const { parentNode, currentNode } = this.findNodeWithParent(value);
+
+    if (!currentNode) return null;
+
+    // removed node
+    const removedNode = Object.assign({}, currentNode, {
+      left: null,
+      right: null,
+    });
+
+    // parent has no children
+    if (!currentNode.left && !currentNode.right) {
+      // we have to do a few things here:
+      // check if parent is the root
+      if (!parentNode) this.root = null;
+      // check if it has left child and if it's true, then it will be null
+      else if (parentNode.left && parentNode.left.value === value)
+        parentNode.left = null;
+      // check if it has right child
+      else if (parentNode.right && parentNode.right.value === value)
+        parentNode.right = null;
+    }
+
+    // parent has two children
+    if (currentNode.left && currentNode.right) {
+      const { nextBigNodeParent, nextBigNode } =
+        this.findNextBigNodeWithParent();
+      // assign the value of the nextBigNode to the current node
+      currentNode.value = nextBigNode.value;
+
+      // Node is the direct parent of the next big node
+      if (nextBigNodeParent === currentNode)
+        nextBigNodeParent.right = nextBigNode.right;
+      // Node is no direct parent of the next biggest node
+      else nextBigNodeParent.left = nextBigNode.right;
+    }
+    // parent has only one child
+    else {
+      const nextNode = currentNode.left || currentNode.right;
+      currentNode.value = nextNode.value;
+      currentNode.left = nextNode.left;
+      currentNode.right = nextNode.right;
+    }
+    return removedNode;
+  }
+
+  findMax(node = this.root) {
+    if (!node) return null;
+
+    let currentNode = node;
+
+    while (currentNode.right) {
+      currentNode = currentNode.right;
+    }
+
+    return currentNode;
+  }
+
+  findSecondLargest(node = this.root) {
+    if (!node) return null;
+
+    let currentNode = node;
+    let parent;
+
+    while (currentNode.right) {
+      parent = currentNode;
+      currentNode = currentNode.right;
+    }
+
+    return currentNode.left ? this.findMax(currentNode.left) : parent;
+  }
+
+  isBalanced(node = this.root) {
+    if (!node) return true;
+
+    const stack = [];
+    const dephts = new Map();
+    stack.push([node, false]);
+
+    while (stack.length) {
+      const [currentNode, seen] = stack.pop();
+      console.log(stack.pop());
+
+      if (!seen) {
+        stack.push([currentNode, true]);
+        if (currentNode.right) stack.push([currentNode.right, 0]);
+        if (currentNode.left) stack.push([currentNode.left, 0]);
+      } else {
+        const left = dephts.get(currentNode.left) || 0;
+        const right = dephts.get(currentNode.right) || 0;
+
+        if (Math.abs(left - right) > 1) return false;
+        dephts.set(currentNode, Math.max(left, right) + 1);
+      }
+    }
+    return true;
   }
 }
+// cN:    10
+// seen:  false
+// stack: [10, true], [12, 0], [9, 0];
+// map:
+
+//              10
+//             /  \
+//           9     12
+//         /  \   / \
+//        5   9  11  20
+//      /  \        /  \
+//     2    6      18  25
 
 let bst = new BinarySearchTree();
 bst.insert(10);
@@ -137,6 +270,12 @@ bst.insert(9);
 bst.insert(12);
 bst.insert(11);
 bst.insert(20);
+bst.insert(18);
+bst.insert(25);
+bst.insert(2);
+bst.insert(6);
+
+console.log(bst.isBalanced());
 
 // bst.insert(15);
 // bst.insert(20);
@@ -146,12 +285,7 @@ bst.insert(20);
 // bst.insert(5);
 // bst.insert(50);
 
-// console.log(bst.DFSPreOrder());
-// console.log(bst.BFS());
-console.log(bst.findNodeWithParent(12));
-
-//              10
-//             /  \
-//           8     12
-//         /  \   / \
-//        5   9  11  20
+// console.log(bst.root.right.right);
+// console.log(bst.remove(6));
+// console.log(bst.remove1(8));
+// console.log(bst.DFSInOrder());
